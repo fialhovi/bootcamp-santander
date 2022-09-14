4e18cc7b5c4657078eeccd8bfb26f7b022f6f4a5
fialhovi <104800356+fialhovi@users.noreply.github.com>
2022-09-14 18:25:55 -0300

    Update 01. Tipos de variáveis.py

--- Conteúdo/01. Python Basics/01. Tipos de variáveis.py
+++ Conteúdo/01. Python Basics/01. Tipos de variáveis.py
@@ -3,18 +3,15 @@ x = 5
 print(f'x = {x}  ', type(x))
 print()
 
-
 # Variavel preco recebe um valor float:
 preco = 6.74
 print(f'Preço vale R$ {preco}', type(preco))
 print()
 
-
 # Variavel cidade recebe um valor string:
 cidade = 'Rio de Janeiro'
 print(f'Cidade: {cidade}', type(cidade))
 print()
-
 
 # Variavel disponivel recebe um valor boolean:
 disponivel = True
