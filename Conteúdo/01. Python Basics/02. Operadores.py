61d1188251f4ec1f3bb90f87a211f7fa2c63f895
fialhovi <104800356+fialhovi@users.noreply.github.com>
2022-09-14 18:37:14 -0300

    Add files via upload

---
+++ Conteúdo/01. Python Basics/02. Operadores.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Variavel x e y recebem valores diferentes:
+x = 50
+y = 2
+
+
+# In[2]:
+
+
+# Somando:
+print(f'Somando: x + y = {x + y}')
+print()
+
+
+# In[3]:
+
+
+# Subtraindo:
+print(f'Subtraindo: x - y = {x - y}')
+print()
+
+
+# In[4]:
+
+
+# Multiplicando:
+print(f'Multiplicando: x * y = {x * y}')
+print()
+
+
+# In[5]:
+
+
+# Dividindo:
+print(f'Dividindo: x / y = {x / y}')
+print()
+
+
+# In[6]:
+
+
+# Potência:
+print(f'Potência: x ** y = {x ** y}')
+print()
+
+
+# In[7]:
+
+
+# Parte inteira da Divisão:
+print(f'Parte inteira da Divisão: x // y = {x // y}')
+print()
+
+
+# In[8]:
+
+
+# Resto da Divisão:
+print(f'Resto da Divisão: x % y = {x % y}')
+print()
+
+
+# In[9]:
+
+
+# Operadores Lógicos:
+print('Operadores Lógicos:')
+tem_cafe = True
+tem_pao = False
+print(f'Não tem café: {not tem_cafe}')
+print(f'Tem café ou pão: {tem_cafe or tem_pao}')
+print(f'Tem café e pão: {tem_cafe and tem_pao}')
+print()
+
+
+# In[10]:
+
+
+dolar = 5.6
+real = 1
+print(f'Dolar é maior que real: {dolar > real}')
+print(f'Dolar é menor que real: {dolar < real}')
+print(f'Dolar é igual ao real: {dolar == real}')
+print(f'Dolar é maior ou igual o real: {dolar >= real}')
+print(f'Dolar é menor ou igual o real: {dolar <= real}')
+print(f'Dolar é diferente do real: {dolar != real}')
+
+
+# In[ ]:
+
+
+
+
