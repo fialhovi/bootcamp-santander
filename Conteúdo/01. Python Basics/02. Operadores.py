b1065293b0b84ba04f8b8512ca669fee93dbfd00
fialhovi <104800356+fialhovi@users.noreply.github.com>
2022-09-14 18:39:19 -0300

    Update 02. Operadores.py

--- Conteúdo/01. Python Basics/02. Operadores.py
+++ Conteúdo/01. Python Basics/02. Operadores.py
@@ -1,71 +1,34 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 # Variavel x e y recebem valores diferentes:
 x = 50
 y = 2
-
-
-# In[2]:
-
 
 # Somando:
 print(f'Somando: x + y = {x + y}')
 print()
 
-
-# In[3]:
-
-
 # Subtraindo:
 print(f'Subtraindo: x - y = {x - y}')
 print()
-
-
-# In[4]:
-
 
 # Multiplicando:
 print(f'Multiplicando: x * y = {x * y}')
 print()
 
-
-# In[5]:
-
-
 # Dividindo:
 print(f'Dividindo: x / y = {x / y}')
 print()
-
-
-# In[6]:
-
 
 # Potência:
 print(f'Potência: x ** y = {x ** y}')
 print()
 
-
-# In[7]:
-
-
 # Parte inteira da Divisão:
 print(f'Parte inteira da Divisão: x // y = {x // y}')
 print()
 
-
-# In[8]:
-
-
 # Resto da Divisão:
 print(f'Resto da Divisão: x % y = {x % y}')
 print()
-
-
-# In[9]:
 
 
 # Operadores Lógicos:
@@ -78,9 +41,7 @@ print(f'Tem café e pão: {tem_cafe and tem_pao}')
 print()
 
 
-# In[10]:
-
-
+# Operadores Relacionais:
 dolar = 5.6
 real = 1
 print(f'Dolar é maior que real: {dolar > real}')
@@ -89,10 +50,3 @@ print(f'Dolar é igual ao real: {dolar == real}')
 print(f'Dolar é maior ou igual o real: {dolar >= real}')
 print(f'Dolar é menor ou igual o real: {dolar <= real}')
 print(f'Dolar é diferente do real: {dolar != real}')
-
-
-# In[ ]:
-
-
-
-
