d1242376206494aa6f5530770f024a49a694e320
fialhovi <104800356+fialhovi@users.noreply.github.com>
2022-09-14 18:49:38 -0300

    Update 04. Estruturas condicionais.py

--- Conteúdo/01. Python Basics/04. Estruturas condicionais.py
+++ Conteúdo/01. Python Basics/04. Estruturas condicionais.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 valor_passagem = 4.30
 valor_corrida = float(input("Qual é o valor da corrida? "))
 
@@ -12,8 +6,6 @@ if valor_corrida <= valor_passagem * 5:
 else:
     print('Pegue o ônibus')
 
-
-# In[2]:
 
 
 valor_passagem = 4.30
@@ -25,10 +17,3 @@ elif valor_corrida <= valor_passagem * 6:
     print('Aguarde alguns minutos e verifique novamente o preço do Uber')
 else: 
     print("Pegue o ônibus")
-
-
-# In[ ]:
-
-
-
-
