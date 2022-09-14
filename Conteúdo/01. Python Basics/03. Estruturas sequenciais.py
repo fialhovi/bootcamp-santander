3ba76c45e6e099ee65327b687cd067c4ed48284e
fialhovi <104800356+fialhovi@users.noreply.github.com>
2022-09-14 18:44:19 -0300

    Add files via upload

---
+++ Conteúdo/01. Python Basics/03. Estruturas sequenciais.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Estruturas Sequenciais:
+idade = int(input('Digite sua idade: '))
+print(f'Idade {idade}', type(idade))
+
+
+# In[2]:
+
+
+print(float("123.25"),type(float('123.25')))
+print(float("123.25"),type(str(123.24)))
+print(bool(''))
+print(bool('abc'))
+print(bool(0))
+print(bool(-2))
+
+
+# In[3]:
+
+
+salario_mensal = float(input("Digite o valor do seu salário mensal: "))
+gasto_mensal = float(input("Digite o valor do seu gasto mensal em média: "))
+
+salario_total = salario_mensal * 12
+gasto_total = gasto_mensal * 12
+montante_economizado = salario_total - gasto_total
+print("O montante que você pode economizar ao fim do ano é de: ", montante_economizado)
+
+
+# In[ ]:
+
+
+
+
