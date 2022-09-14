513dcdea62d8f0644424149bfaa676e2041281ec
fialhovi <104800356+fialhovi@users.noreply.github.com>
2022-09-14 18:24:24 -0300

    Add files via upload

---
+++ Conteúdo/01. Python Basics/01. Tipos de variáveis.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Variavel x recebe um valor inteiro:
+x = 5
+print(f'x = {x}  ', type(x))
+print()
+
+
+# In[2]:
+
+
+# Variavel preco recebe um valor float:
+preco = 6.74
+print(f'Preço vale R$ {preco}', type(preco))
+print()
+
+
+# In[3]:
+
+
+# Variavel cidade recebe um valor string:
+cidade = 'Rio de Janeiro'
+print(f'Cidade: {cidade}', type(cidade))
+print()
+
+
+# In[5]:
+
+
+# Variavel disponivel recebe um valor boolean:
+disponivel = True
+print(f'Disponível: {disponivel}', type(disponivel))
+
+
+# In[ ]:
+
+
+
+
