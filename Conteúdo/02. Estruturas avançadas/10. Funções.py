b8864c5fac1a4ea6ed823fe896d0ec926148a88a
fialhovi <104800356+fialhovi@users.noreply.github.com>
2022-09-15 11:49:18 -0300

    Update 10. Funções.py

--- Conteúdo/02. Estruturas avançadas/10. Funções.py
+++ Conteúdo/02. Estruturas avançadas/10. Funções.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 # Funções:
 
 # Declarando uma função sem parêmetro:
@@ -14,24 +8,14 @@ def hello():
 hello()
 
 
-# In[3]:
-
-
 # Declarando uma função com parâmetro que calcula a média aritmética de três notas:
 def calcula_media(valor1, valor2, valor3):
     soma = valor1 + valor2 + valor3
     media = soma / 3
     return media
 
-
-# In[4]:
-
-
 nota = calcula_media(8.5, 4.9, 10)
 print(f'Média: {nota}')
-
-
-# In[5]:
 
 
 # Funções recursivas:
@@ -41,15 +25,9 @@ def funcaoRecursiva(numero):
         funcaoRecursiva(numero - 1)
     print(numero)
 
-
-# In[6]:
-
-
 print("Testando a função recursiva:")
 funcaoRecursiva(10)
 
-
-# In[7]:
 
 
 # Funções II
@@ -61,14 +39,7 @@ def calcula_media1(*args):
     media = soma / len(args)
     return media
 
-
-# In[8]:
-
-
 print(f'Média de vários parâmetros: {calcula_media1(10, 5, 8, 9, 10, 7, 6, 2)}')
-
-
-# In[9]:
 
 
 # Funções com vários parâmetros: quando utilizamos dois ** em parâmetros, quer dizer que podemos passar vários argumentos, que são armazenados em
@@ -76,15 +47,4 @@ print(f'Média de vários parâmetros: {calcula_media1(10, 5, 8, 9, 10, 7, 6, 2)
 def print_info(**kwargs):
     print(kwargs, type(kwargs))
 
-
-# In[10]:
-
-
 print_info(nome='Vinícius', sobrenome='Fialho')
-
-
-# In[ ]:
-
-
-
-
