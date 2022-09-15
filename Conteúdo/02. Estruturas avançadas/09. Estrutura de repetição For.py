57562a848d9712adcda096fbe8ddfa138f991009
fialhovi <104800356+fialhovi@users.noreply.github.com>
2022-09-15 11:38:38 -0300

    Update 09. Estrutura de repetição For.py

--- Conteúdo/02. Estruturas avançadas/09. Estrutura de repetição For.py
+++ Conteúdo/02. Estruturas avançadas/09. Estrutura de repetição For.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 # Percorrendo coleções
 # O for é um tipo especial de loop feito para percorrer elementos de uma coleção. Antes, vimos exemplos usando um while e um contador para percorrer uma lista.
 # O for elimina a necessidade de inicializarmos um contador, incrementarmos e verificar a condição de parada. Sintaxe:
@@ -15,17 +9,9 @@ nomes_cidade = ['São Paulo', 'Londres', 'Tóquio', 'Paris']
 for cidades in nomes_cidade:
     print(f'- {cidades}')
 
-
-# In[2]:
-
-
 #criando uma lista com a numeração de cada elemento nela:
 for numeros in enumerate(nomes_cidade):
     print(f'{numeros}')
-
-
-# In[3]:
-
 
 # Usando for em dicionários:
 # Criando um dicionário:
@@ -35,17 +21,9 @@ cidade = {
     'população_milhoes': 12.2
 }
 
-
-# In[4]:
-
-
 # chave acessa somente a chave estado, cidade e população_milhoes:
 for chave in cidade:
     print(f'{chave}: {cidade[chave]}')
-
-
-# In[5]:
-
 
 # Função range(), cria uma lista começando do 0 até o elemento indicado -1:
 print(list(range(10)))
@@ -53,10 +31,3 @@ print((list(range(2, 10))))
 
 #Podemos especificar o valor inicial, o final e o incremento:
 print(list(range(2, 10, 2)))
-
-
-# In[ ]:
-
-
-
-
