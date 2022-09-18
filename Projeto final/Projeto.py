e793c4773ef7cb615af1f00824ff61b4697c56e1
fialhovi <104800356+fialhovi@users.noreply.github.com>
2022-09-18 11:13:32 -0300

    Update Projeto.py

--- Projeto final/Projeto.py
+++ Projeto final/Projeto.py
@@ -1,44 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 #P/ começar, importar a biblioteca 'request', dando o apelido 'r', para facilitar a referenciação:
 import requests as r
-
-
-# In[2]:
-
 
 #Chamando a bilbioteca para essa url:
 url = 'https://api.covid19api.com/dayone/country/brazil'
 resp = r.get(url)
 
-
-# In[3]:
-
-
 #Vamos ver o status dessa requisição para ver se deu tudo certo:
 resp.status_code
-
-
-# In[4]:
-
 
 #guardar os dados na variável raw data:
 raw_data = resp.json()
 
-
-# In[5]:
-
-
 #Vamos ver o que tem lá na raw_data, pegar a posição 0 já que é uma lista com essas informações:
 raw_data[0]
-
-
-# In[6]:
-
 
 #Para o nosso objetivo, tem algumas coisas que não precisamos armazenar (ex. país, código do país, latitude etc.)
 #Vamos filtrar apenas as informações que vamos precisar:
@@ -46,17 +20,9 @@ final_data = []
 for obs in raw_data:
     final_data.append([obs['Confirmed'],obs['Deaths'],obs['Recovered'],obs['Active'],obs['Date']])
 
-
-# In[7]:
-
-
 #Vamos adicionar os headers, para identificar o que é cada item:
 final_data.insert(0, ['Confirmados', 'Obitos', 'Recuperados', 'Ativos', 'Data'])
 final_data
-
-
-# In[8]:
-
 
 #Vamos criar variáveis que serão constantes para a limpeza dos dados.
 CONFIRMADOS = 0
@@ -65,26 +31,14 @@ RECUPERADOS = 2
 ATIVOS = 3
 DATA = 4
 
-
-# In[9]:
-
-
 #Agora, vamos sobrescrever algumas 'obs' que temos em final_data, então vamos pegar as posições.
 #Aqui, retirando os minutos, horas e segundos da data, que não importam para a nossa análise:
 for i in range(1, len(final_data)):
     final_data[i][DATA] = final_data[i][DATA][:10]
 final_data
 
-
-# In[10]:
-
-
 #Vamos transformar a string de data para o tipo Data de verdade:
 import datetime as dt
-
-
-# In[11]:
-
 
 # DateTime: Data, hora, segundo e microsegundo:
 print(dt.time(12, 6, 21, 7), 'Hora:minuto:segundo:microsegundo')
@@ -92,10 +46,6 @@ print('----')
 print(dt.date(2021, 10, 30), 'Ano/mês/dia')
 print('----')
 print(dt.datetime(2021, 10, 30, 12, 6, 21, 7), 'Ano/mês/dia Hora:minuto:segundo:microsegundo')
-
-
-# In[12]:
-
 
 # Tipos de datas contadores de segundos:
 
@@ -106,38 +56,18 @@ print(f'Faltam {(reveillon - natal).days} dias para o reveillon')
 print(f'Faltam {(reveillon - natal).total_seconds()} segundos para o reveillon')
 print(f'Faltam {(reveillon - natal).microseconds} microsegundos para o reveillon')
 
-
-# In[13]:
-
-
 # Transformando os dados da minha API em csv:
 import csv
-
-
-# In[14]:
-
 
 with open('brasil-covid.csv', 'w') as file:
     writer = csv.writer(file)
     writer.writerows(final_data)
 
-
-# In[15]:
-
-
 # Alterando a data dos dados que é string para datetime:
 for i in range(1, len(final_data)):
     final_data[i][DATA] = dt.datetime.strptime(final_data[i][DATA], '%Y-%m-%d')
 
-
-# In[16]:
-
-
 final_data
-
-
-# In[17]:
-
 
 # Criando uma função para construir os dados do eixo y:
 def get_datasets(y, labels):
@@ -157,10 +87,6 @@ def get_datasets(y, labels):
             }
         ]
 
-
-# In[18]:
-
-
 # Função para definir o titulo do gráfico:
 def set_title(title=''):
     if title != '':
@@ -171,10 +97,6 @@ def set_title(title=''):
         'title': title,
         'display': display
     }
-
-
-# In[19]:
-
 
 # Função que cria o nosso dicionário, que cria o gráfico:
 def create_chart(x, y, labels, kind='bar', title=''):
@@ -193,19 +115,11 @@ def create_chart(x, y, labels, kind='bar', title=''):
     
     return chart
 
-
-# In[20]:
-
-
 # Função para fazer a requisição na API: o retorno é binário
 def get_api_chart(chart):
     url_base = 'https://quickchart.io/chart'
     resp = r.get(f'{url_base}?c={str(chart)}')
     return resp.content
-
-
-# In[21]:
-
 
 # Função para salvar a imagem:
 def save_image(path, content):
@@ -213,29 +127,19 @@ def save_image(path, content):
         #o 'wb' indica que é uma escrita binária.
         image.write(content)
 
-
-# In[22]:
-
-
 #Importar bibliotecas e suas funções:
 get_ipython().system('pip install Pillow')
 
 from PIL import Image
 from IPython.display import display
 
-
-# In[23]:
-
-
 # Função para mostrar a imagem
 def display_image(path):
     img_pil = Image.open(path)
     display(img_pil)
 
-
-# In[29]:
-
-
+    
+    
 y_data_1 = []
 for obs in final_data[1::30]:
     y_data_1.append(obs[CONFIRMADOS])
@@ -255,16 +159,8 @@ chart_content = get_api_chart(chart)
 save_image('meu-primeiro-grafico.png', chart_content)
 display_image('meu-primeiro-grafico.png')
 
-
-# In[30]:
-
-
 #importar mais função para gerarmos um qr code para o nosso gráfico:
 from urllib.parse import quote
-
-
-# In[31]:
-
 
 # Gerar QR-CODE:
 def get_api_qrcode(link):
@@ -273,19 +169,8 @@ def get_api_qrcode(link):
     resp = r.get(f'{url_base}?text={text}')
     return resp.content
 
-
-# In[32]:
-
-
 # Recuperar o link
 url_base = 'https://quickchart.io/chart'
 link = f'{url_base}?c={str(chart)}'
 save_image('qr-code.png', get_api_qrcode(link))
 display_image('qr-code.png')
-
-
-# In[ ]:
-
-
-
-
