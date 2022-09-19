ff4c42323a17972349442e30ab7a0d6df4620d9f
fialhovi <104800356+fialhovi@users.noreply.github.com>
2022-09-18 21:15:51 -0300

    Update Módulo 2.py

--- Exercícios/Módulo 2.py
+++ Exercícios/Módulo 2.py
@@ -49,3 +49,28 @@ soma_lista([1,4,3], [3,5,1])
 
 #QUESTÃO 4
 #Crie um dicionário cujas chaves são os meses do ano e os valores são a duração (em dias) de cada mês.
+
+
+meses_ano = {
+    'Janeiro': 31,
+    'Fevereiro': 28,
+    'Março': 31,
+    'Abril': 30,
+    'Maio': 31,
+    'Junho': 30,
+    'Julho': 31,
+    'Agosto': 31,
+    'Setembro': 30,
+    'Outubro': 31,
+    'Novembro': 30,
+    'Dezembro': 31
+}
+
+
+
+
+
+#QUESTÃO 5
+#Imprima as chaves seguidas dos seus valores para dicionário criado no exercício anterior.
+
+
